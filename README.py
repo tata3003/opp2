960109b732af5e30d9916b05c4e0cfbd64597d10
Zena <zena3003ta@mail.ru>
2024-05-08 21:56:54 +0300

    first commit

---
+++ README.py
@@ -0,0 +1,41 @@
+cook_book = {
+  'Омлет': [
+    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    ],
+  'Утка по-пекински': [
+    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    ],
+  'Запеченный картофель': [
+    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    ]
+}
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingredient_name = ingredient['ingredient_name']
+                quantity = ingredient['quantity'] * person_count
+                measure = ingredient['measure']
+                
+                if ingredient_name not in shop_list:
+                    shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
+                else:
+                    shop_list[ingredient_name]['quantity'] += quantity
+    
+    return shop_list
+
+# Пример использования функции
+dishes = ['Омлет', 'Утка по-пекински', 'Запеченный картофель']
+person_count = 2
+result = get_shop_list_by_dishes(dishes, person_count)
+print(result)
